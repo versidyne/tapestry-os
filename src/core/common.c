06c2fa72904041526571967bbf8b5ee40f9ae102
alexgurrola <alex.gurrola@versidyne.com>
2013-06-16 18:32:31 -0700

    Removed comment

--- src/core/common.c
+++ src/core/common.c
@@ -57,7 +57,6 @@ int strcmp(char *str1, char *str2)
       }
       return 0;
       
-      // Original code
       /*int i = 0;
       int failed = 0;
       while(str1[i] != '\0' && str2[i] != '\0')
